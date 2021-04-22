65c53cc1cfdbbe94922d669737d796c2063cc5e3
SzymonKubica <szymo.kubica@gmail.com>
2021-04-22 21:34:21 +0200

    Added a stress test for coarse grained set and tested it. Also checked that sequential set fails this test.

--- test/adts/CoarseGrainedLinkedSetTest.java
+++ test/adts/CoarseGrainedLinkedSetTest.java
@@ -24,7 +24,7 @@ public class CoarseGrainedLinkedSetTest {
 
   @Test
   void sizeTest() {
-    SequentialSet<Integer> integers = getSampleIntegerSet();
+    CoarseGrainedLinkedSet<Integer> integers = getSampleIntegerSet();
     Assertions.assertEquals(5, integers.size());
   }
 
@@ -34,5 +34,33 @@ public class CoarseGrainedLinkedSetTest {
       integers.add(i);
     }
     return integers;
+  }
+
+  @Test
+  void stressTest() {
+    CoarseGrainedLinkedSet<Integer> integers = new CoarseGrainedLinkedSet<>();
+    Thread thread1 = new Thread(() -> {
+      for (int i = 0; i < 1000; i++) {
+        integers.add(i);
+      }
+    });
+
+    Thread thread2 = new Thread(() -> {
+      for (int i = 1000; i < 2000; i++) {
+        integers.add(i);
+      }
+    });
+
+    thread1.start();
+    thread2.start();
+
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      System.out.println(e.getMessage());
+    }
+
+    Assertions.assertEquals(2000, integers.size());
   }
 }
